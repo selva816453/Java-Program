7441b0254ea66f4349af720bea5ef80d71dbbd5c
selva816453 <selva816453@gmail.com>
2026-08-22 18:09:51 +0530

    Check Divisibility by Digit Sum and Product

---
+++ Check Divisibility by Digit Sum and Product.java
@@ -0,0 +1,39 @@
+// You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
+// The digit sum of n (the sum of its digits).
+// The digit product of n (the product of its digits).
+// Return true if n is divisible by this sum; otherwise, return false.
+
+ 
+
+// Example 1:
+// Input: n = 99
+// Output: true
+// Explanation:
+// Since 99 is divisible by the sum (9 + 9 = 18) plus product (9 * 9 = 81) of its digits (total 99), the output is true.
+
+// Example 2:
+// Input: n = 23
+// Output: false
+// Explanation:
+// Since 23 is not divisible by the sum (2 + 3 = 5) plus product (2 * 3 = 6) of its digits (total 11), the output is false.
+
+ 
+
+// Constraints:
+// 1 <= n <= 106
+
+
+class Check Divisibility by Digit Sum and Product{
+    public boolean checkDivisibility(int n) {
+        int temp=n;
+        int sum=0;
+        int mul=1;
+        while(temp!=0){
+            int d=temp%10;
+            sum+=d;
+            mul*=d;
+            temp/=10;
+        }
+        return n%(mul+sum)==0;
+    }
+}
