c8b3dd083c9666146b1ce526d03defc93cec5bf6
Selvam M <selva816453@gmail.com>
2026-08-28 20:28:19 +0530

    Fix comment formatting in Array Search.java

--- Array Search.java
+++ Array Search.java
@@ -1,4 +1,4 @@
-// Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+ // Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
 
 // Examples:
 
