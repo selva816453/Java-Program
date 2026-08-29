476a58a92f8ac83afdf0b1d3b34768aeb6e0de3a
Selvam M <selva816453@gmail.com>
2026-08-29 21:34:32 +0530

    Fix comment formatting in Check Divisibility file

--- Check Divisibility by Digit Sum and Product.java
+++ Check Divisibility by Digit Sum and Product.java
@@ -1,4 +1,4 @@
-// You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
+ // You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
 // The digit sum of n (the sum of its digits).
 // The digit product of n (the product of its digits).
 // Return true if n is divisible by this sum; otherwise, return false.
