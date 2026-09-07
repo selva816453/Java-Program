37bcc9caba30696eddf2611c508ff8a1f8ee2ab7
selva816453 <selva816453@gmail.com>
2026-09-07 20:55:28 +0530

    Check Good Integer

---
+++ Check Good Integer.java
@@ -0,0 +1,43 @@
+// You are given a positive integer n.
+// Let digitSum be the sum of the digits of n, and let squareSum be the sum of the squares of the digits of n.
+// An integer is called good if squareSum - digitSum >= 50.
+// Return true if n is good. Otherwise, return false.
+
+ 
+
+// Example 1:
+// Input: n = 1000
+// Output: false
+// Explanation:
+// The digits of 1000 are 1, 0, 0, and 0.
+// The digitSum is 1 + 0 + 0 + 0 = 1.
+// The squareSum is 12 + 02 + 02 + 02 = 1.
+// The squareSum - digitSum is 1 - 1 = 0. As 0 is not greater than or equal to 50, the output is false.
+
+// Example 2:
+// Input: n = 19
+// Output: true
+// Explanation:
+// The digits of 19 are 1 and 9.
+// The digitSum is 1 + 9 = 10.
+// The squareSum is 12 + 92 = 1 + 81 = 82.
+// The squareSum - digitSum is 82 - 10 = 72. As 72 is greater than or equal to 50, the output is true.
+ 
+
+// Constraints:
+// 1 <= n <= 109
+
+
+class Check Good Integer{
+    public boolean checkGoodInteger(int n) {
+        int sum=0;
+        int square=0;
+        while(n!=0){
+            int d=n%10;
+            sum+=d;
+            square+=d*d;
+            n/=10;
+        }
+        return Math.abs(sum-square)>=50;
+    }
+}
